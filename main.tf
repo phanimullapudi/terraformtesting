d3894c45ce6841e3922756cb03070587b5a26455
Phani Mullapudi <phmullap@adobe.com>
2019-11-15 17:02:53 -0600

    ASG testing

--- main.tf
+++ main.tf
@@ -3,20 +3,32 @@ variable "server_port" {
     default = 8080
 }
 
-resource "aws_instance" "example" {
-    ami             = "ami-6f68cf0f"
+data "aws_availability_zones" "all" {}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = "${aws_launch_configuration.example.id}"
+    availability_zones = ["us-west-2a", "us-west-2b"]
+    min_size = 2
+    max_size = 2
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true  
+    }
+}
+
+
+resource "aws_launch_configuration" "example" {
+    image_id             = "ami-6f68cf0f"
     instance_type   = "t2.micro"
-    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+    security_groups = ["${aws_security_group.instance.id}"]
 
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hellow World" > index.html
                 nohup busybox httpd -f -p 8080 &
                 EOF
-
-        tags = {
-        Name = "terraform-example"
-    } 
 
     lifecycle {
         create_before_destroy = true
@@ -33,9 +45,4 @@ resource "aws_security_group" "instance" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-}
-
-output "public_ip" {
-    value = "${aws_instance.example.public_ip}"
-
 }
