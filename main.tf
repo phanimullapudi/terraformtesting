c2916da4051a1b7ec3f01ba900e1a0f9318d97c8
Phani Mullapudi <phmullap@adobe.com>
2019-11-16 09:07:22 -0600

    Integration with Terragrunt

--- main.tf
+++ main.tf
@@ -1,13 +1,48 @@
+terraform {
+  backend "s3" {}
+}
+
 variable "server_port" {
     description = "The port the server will use for HTTP request"
     default = 8080
 }
+
+
+resource "aws_elb" "example" {
+    name                = "terraform-asg-example"
+    availability_zones  = ["us-west-2a", "us-west-2b"]
+    security_groups     = ["${aws_security_group.elb.id}"]
+
+
+    listener {
+        lb_port             = 80
+        lb_protocol         = "http"
+        instance_port       = "${var.server_port}"
+        instance_protocol   = "http" 
+    }
+
+    health_check {
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+        timeout             = 3
+        interval            = 30
+        target              = "HTTP:${var.server_port}/"
+     
+    }
+}
+
+
+
 
 data "aws_availability_zones" "all" {}
 
 resource "aws_autoscaling_group" "example" {
     launch_configuration = "${aws_launch_configuration.example.id}"
     availability_zones = ["us-west-2a", "us-west-2b"]
+
+    load_balancers      = ["${aws_elb.example.name}"]
+    health_check_type   = "ELB"
+
     min_size = 2
     max_size = 2
 
@@ -46,3 +81,25 @@ resource "aws_security_group" "instance" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "elb" {
+    name = "terraform-example-instance-1"
+
+    ingress {
+        from_port   = "${var.server_port}"
+        to_port     = "${var.server_port}"
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+    output "elb_dns_name" {
+        value   = "${aws_elb.example.dns_name}"
+    }
